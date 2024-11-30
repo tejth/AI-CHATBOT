fdfddabfe9b65188e552846449b33fd7534541d5
tejth <ptejendra91@gmail.com>
2024-11-30 22:34:25 +0530

    setting up navbar with differnet routes on frontend

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Link, Box, Typography, useTheme } from "@mui/material";
+const Navbar = () => {
+  const theme = useTheme();
+  return (
+    <Box
+      width={"100%"}
+      backgroundColor={theme.palette.background.alt}
+      p="1rem 6%"
+      textAlign={"center"}
+      sx={{ boxShadow: 3, mb: 2 }}
+    >
+      <Typography variant="h1" color="primary" fontWeight="bold">
+        AI Chatbot
+      </Typography>
+      <Link href="/register" p={1}>
+        Sign Up
+      </Link>
+      <Link href="/login" p={1}>
+        Sign In
+      </Link>
+    </Box>
+  );
+};
+
+export default Navbar;
